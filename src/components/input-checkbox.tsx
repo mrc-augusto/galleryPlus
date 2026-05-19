44a5a28de2dda72c5fc3e40de0da89aca0dac598
Maurício Augusto <mrc_augusto@hotmail.com>
2026-05-19 18:16:54 -0300

    feat: add input text and checkbox components

---
+++ src/components/input-checkbox.tsx
@@ -0,0 +1,77 @@
+import Icon from "./icon";
+import CheckIcon from "../assets/icons/check.svg?react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+export const inputCheckboxWrapperVariants = tv({
+  base: "inline-flex items-center justify-center relative group",
+});
+
+export const inputCheckboxVariants = tv({
+  base: `
+      appearance-none peer flex items-center justify-center
+      cursor-pointer transition overflow-hidden border
+    `,
+  variants: {
+    variant: {
+      default: `
+            border-border-primary hover:border-border-active
+            checked:border-accent-brand checked:bg-accent-brand
+            group-hover:checked:border-accent-brand-light
+            group-hover:checked:bg-accent-brand-light
+          `,
+    },
+    size: {
+      sm: "w-3 h-3 rounded-sm",
+      md: "w-5 h-5 rounded-sm",
+    },
+    disabled: {
+      true: "pointer-events-none",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "md",
+    disabled: false,
+  },
+});
+
+export const inputCheckboxIconVariants = tv({
+  base: `
+      absolute top-1/2 -translate-y-1/2
+      hidden peer-checked:block fill-white
+      cursor-pointer
+    `,
+  variants: {
+    size: {
+      sm: "w-3 h-3 left-px",
+      md: "w-4 h-4 left-0.5",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface InputCheckboxProps
+  extends
+    VariantProps<typeof inputCheckboxVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {}
+
+export function InputCheckbox({
+  size,
+  className,
+  disabled,
+  variant,
+  ...props
+}: InputCheckboxProps) {
+  return (
+    <label className={inputCheckboxWrapperVariants({className})}>
+      <input
+        type="checkbox"
+        className={inputCheckboxVariants({ variant, size, disabled })}
+        {...props}
+      />
+      <Icon svg={CheckIcon} className={inputCheckboxIconVariants({ size })} />
+    </label>
+  );
+}
