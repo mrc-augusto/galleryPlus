5a16e01378198bdd53347199a9244ebb3ffbb9e1
Maurício Augusto <mrc_augusto@hotmail.com>
2026-05-27 15:50:30 -0300

    feat: replace PhotoWidget with PhotosList component in PageHome

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,52 +1,12 @@
-import Container from "../components/container"
-import { PhotoWidget } from "../contexts/photos/components/photo-widget"
-import type { Photo } from "../contexts/photos/models/photo"
+import Container from "../components/container";
+import { PhotosList } from "../contexts/photos/components/photos-list";
 
-export function PageHome(){
-  return(
+export function PageHome() {
+  return (
     <Container>
-      <div className='grid grid-cols-4 gap-9'>
-        <PhotoWidget
-          photo={{
-            id: '123',
-            title: 'Olá, sou uma foto',
-            imageId: 'portrait-tower.png',
-            albums: [
-              {id: '1', title: 'Album 1'},
-              {id: '2', title: 'Album 2'},
-              {id: '3', title: 'Album 3'},
-            ]
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '456',
-            title: 'Olá, sou uma foto',
-            imageId: 'portrait-tower.png',
-            albums: [
-              {id: '1', title: 'Album 1'},
-              {id: '2', title: 'Album 2'},
-              {id: '3', title: 'Album 3'},
-            ]
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: '789',
-            title: 'Olá, sou uma foto',
-            imageId: 'portrait-tower.png',
-            albums: [
-              {id: '1', title: 'Album 1'},
-              {id: '2', title: 'Album 2'},
-              {id: '3', title: 'Album 3'},
-            ]
-          }}
-        />
-        <PhotoWidget
-          photo={{} as Photo}
-          loading
-        />
-      </div>
+      <PhotosList
+        photos={[]}
+      />
     </Container>
-  )
+  );
 }
